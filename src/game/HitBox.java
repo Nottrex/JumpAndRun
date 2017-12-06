c730d222afe317d71b8614c093ec7f80d2df5a71
Nottrex <Lau.Nottrex@gmail.com>
2017-12-06 18:09:40 +0100

    [+] Hitbox from x1, y1, x2, y2 to x, y, width, height

--- src/game/HitBox.java
+++ src/game/HitBox.java
@@ -1,78 +1,22 @@
 package game;
 
 public class HitBox {
-	private float x1, y1, y2, x2;
+	public float x, y, width, height;
 
-	public HitBox(float x1, float y1, float x2, float y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-		checkValues();
-	}
-
-	public void setX1(float x1) {
-		this.x1 = x1;
-		checkValues();
-	}
-
-	public void setX2(float x2) {
-		this.x2 = x2;
-		checkValues();
-	}
-
-	public void setY1(float y1) {
-		this.y1 = y1;
-		checkValues();
-	}
-
-	public void setY2(float y2) {
-		this.y2 = y2;
-		checkValues();
-	}
-
-	public void setFirstPoint(float x1, float y1) {
-		this.x1 = x1;
-		this.y1 = y1;
-		checkValues();
-	}
-
-	public void setSecondPoint(float x2, float y2) {
-		this.x2 = x2;
-		this.y2 = y2;
-		checkValues();
-	}
-
-	public void setPoints(float x1, float y1, float x2, float y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-		checkValues();
+	public HitBox(float x, float y, float width, float height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 
 	public void move(float mx, float my) {
-		this.x1 += mx;
-		this.y1 += my;
-		this.x2 += mx;
-		this.y2 += my;
-	}
-
-	private void checkValues() {
-		if (x1 > x2) {
-			float temp = this.x1;
-			this.x1 = this.x2;
-			this.x2 = temp;
-		}
-		if (y1 > y2) {
-			float temp = this.y1;
-			this.y1 = this.y2;
-			this.y2 = temp;
-		}
+		this.x += mx;
+		this.y += my;
 	}
 
 	public boolean collides(HitBox box2) {
-		return (x2 > box2.x1 && box2.x2 > x1 && y2 > box2.y1 && box2.y2 > y1);
+		return ((x + width) > box2.x && (box2.x + box2.width) > x && (y + height) > box2.y && (box2.y + box2.height) > y);
 	}
 
 	/**
@@ -87,9 +31,9 @@ public class HitBox {
 	public HitBoxDirection direction(HitBox box2) {
 		if (collides(box2)) return HitBoxDirection.COLLIDE;
 
-		if (x2 <= box2.x1) return HitBoxDirection.RIGHT;
-		if (box2.x2 <= x1) return HitBoxDirection.LEFT;
-		if (y2 <= box2.x1) return HitBoxDirection.DOWN;
+		if ((x + width) <= box2.x) return HitBoxDirection.RIGHT;
+		if ((box2.x + box2.width) <= x) return HitBoxDirection.LEFT;
+		if ((y + height) <= box2.x) return HitBoxDirection.DOWN;
 		return HitBoxDirection.UP;
 	}
 
@@ -106,16 +50,16 @@ public class HitBox {
 
 		if (ax != 0) {
 			if (ax < 0) {
-				distance = (box2.x2 - x1) / (-ax);
+				distance = ((box2.x + box2.width) - x) / (-ax);
 			} else {
-				distance = (x2 - box2.x1) / ax;
+				distance = ((x + width) - box2.x) / ax;
 			}
 		}
 		if (ay != 0) {
 			if (ay < 0) {
-				distance = Math.min((box2.y2 - y1) / (-ay), distance);
+				distance = Math.min(((box2.y + box2.height) - y) / (-ay), distance);
 			} else {
-				distance = Math.min((y2 - box2.y1) / ay, distance);
+				distance = Math.min(((y + height) - box2.y) / ay, distance);
 			}
 		}
 
@@ -124,15 +68,32 @@ public class HitBox {
 
 	@Override
 	protected Object clone() {
-		return new HitBox(x1, y1, x2, y2);
+		return new HitBox(x, y, x + width, y + height);
 	}
 
 	@Override
 	public String toString() {
-		return String.format("[(%f, %f), (%f, %f)]", x1, y1, x2, y2);
+		return String.format("[(%f, %f), (%f, %f)]", x, y, x + width, y + height);
 	}
 
 	private enum HitBoxDirection {
-		LEFT, UP, RIGHT, DOWN, COLLIDE,
+		LEFT(-1, 0), UP(0, 1), RIGHT(1, 0), DOWN(0, -1), COLLIDE(0, 0);
+
+		private float ax, ay;
+
+		HitBoxDirection(float ax, float ay) {
+			this.ax = ax;
+			this.ay = ay;
+		}
+
+		float getXDirection() {
+			if (ax == 0 && ay == 0) return ((float) Math.random()) * 2 - 1;
+			return ax;
+		}
+
+		float getYDirection() {
+			if (ax == 0 && ay == 0) return ((float) Math.random()) * 2 - 1;
+			return ay;
+		}
 	}
 }
