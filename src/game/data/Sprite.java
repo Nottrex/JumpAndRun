47ff7d43c884930c0e13e7c41fbe4af01ae08a16
PhoenixofForce <phoenixofforce@gmail.com>
2018-02-21 11:31:02 +0100

    Update Sprite.java

--- src/game/data/Sprite.java
+++ src/game/data/Sprite.java
@@ -7,25 +7,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sprite {
-	private List<Rectangle> textures;
-	private int time;
+	private List<Rectangle> textures;			//Coordinates on spritesheet from textures
+	private int time;					//Time (in ms) before changing sprite
 
 	public Sprite(int time, String... textureNames) {
 		this.textures = new ArrayList<>();
 		this.time = time;
 
+		//Adding texture coordinates to list
 		for (String texture : textureNames) {
 			textures.add(TextureHandler.getSpriteSheetBounds("textures_" + texture));
 		}
 	}
 
+	/** Gets current sprite of the animation
+	*   @param startTime time when the animation started
+	*   @param currentTime 
+	*   @return coordinates from current sprite
+	**/
 	public Rectangle getTexture(long startTime, long currentTime) {
 		return textures.get((int) (((currentTime - startTime) / time) % textures.size()));
 	}
 
 	@Override
 	public boolean equals(Object b) {
-
 		if(b instanceof Sprite){
 			Sprite s = (Sprite) b;
 			if(s.time != time) return false;
@@ -40,8 +45,6 @@ public class Sprite {
 			}
 			return true;
 		}
-
 		return false;
 	}
-
 }
