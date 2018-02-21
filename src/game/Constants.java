41ace948db32f603bb9db1c59ffd0fbc49c67910
InvictusLooper <github@yannicklehmann.de>
2018-02-21 12:02:21 +0100

    Update Constants.java

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -12,10 +12,10 @@ public class Constants {
 	public static final String WINDOW_NAME = "JumpAndRun";
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
-	public static final float FOV = 90;
-	public static final float NEAR = 0.01f;
-	public static final float FAR = 1000f;
-	public static final int TPS = 60;
+	public static final float FOV = 90;					//field of view
+	public static final float NEAR = 0.01f;					//minimum distance of objects to camera
+	public static final float FAR = 1000f;					//maximum distance of objects to camera
+	public static final int TPS = 60;					//ticks per second
 
 	public static final float JUMP_ACCELERATION = 0.3f;
 	public static final float MAX_WALKING_SPEED = 0.175f;
@@ -27,28 +27,28 @@ public class Constants {
 	public static final float MAX_SPEED = 0.5f;
 
 	public static int PIXEL_PER_TILE = 8;
-	public static final float FONT_ASPECT = 1.4f; 		//FONT_HEIGHT / FONT_WIDTH
-	public static final float FONT_SPACING = 1.2f; 		//FONT_TOTAL_WIDTH / FONT_WIDTH
-	public static final int MAX_PARTICLES = 1000;
+	public static final float FONT_ASPECT = 1.4f; 				//font height : font width
+	public static final float FONT_SPACING = 1.2f; 				//1 + spacing : font width
+	public static final int MAX_PARTICLES = 1000;				//maximum particle count displayed simultaneously
 	public static final int[] INDICES = new int[]{0, 2, 1, 0, 3, 2};
 	public static final float[][] VERTEX_POS = new float[][]{{0, 0}, {0, 1}, {1, 1}, {1, 0}};
 
 	public static final PrintStream ERR_STREAM = System.err;
 	public static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm:ss");
 
-	public static final int TIME_FRAC = 25;
-	public static final float MIN_AMP = 0.0001f;
-	public static final float DECAY = 0.8f;
+	public static final int TIME_FRAC = 25;					//time after which the amplitude reduces by DECAY
+	public static final float DECAY = 0.8f;					//percentage of screenshake after TIME_FRAC
+	public static final float MIN_AMP = 0.0001f;				//minimum amplitude for screenshakes before getting dumped 
 
-	public static final float MIN_CAMERA_ZOOM = 0.125f;
-	public static final int FADE_TIME = 60;
+	public static final float MIN_CAMERA_ZOOM = 0.125f;			//nearest camera distance
+	public static final int FADE_TIME = 60;					//tick amount of camera fading
 
 	public static final String DATA_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator;
-	public static final String DATA_FILE_PATH = DATA_PATH + "options.yml";
-	public static final String SYS_PREFIX = String.valueOf(Math.random());
+	public static final String DATA_FILE_PATH = DATA_PATH + "options.yml";	//directory of settings file
+	public static final String SYS_PREFIX = String.valueOf(Math.random());	//internal prefix for private data
 
-	public static final Map<String, Integer> DEFAULT_CONTROLS = new HashMap<>();
-	public static final float DEAD_ZONE = 0.2f;
+	public static final Map<String, Integer> DEFAULT_CONTROLS = new HashMap<>();	//default controls for all 18 players
+	public static final float DEAD_ZONE = 0.2f;					//dead zone for controller triggers
 
 	static {
 		DEFAULT_CONTROLS.put("UP0", Keyboard.KEY_W);
