09f09cf828218035ca0e3c8b805f588e7e0059d2
Nottrex <Lau.Nottrex@gmail.com>
2018-02-28 12:05:38 +0100

    Update AbstractGameObject.java

--- src/game/gameobjects/AbstractGameObject.java
+++ src/game/gameobjects/AbstractGameObject.java
@@ -2,8 +2,11 @@ package game.gameobjects;
 
 import game.Game;
 
+/**
+ * An implementation of GameObject that stores the game instance
+**/
 public abstract class AbstractGameObject implements GameObject {
-	protected Game game;
+	protected Game game; // The variable used to store the game instance
 
 	@Override
 	public void init(Game game) {
