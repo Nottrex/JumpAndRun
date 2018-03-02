75e22baf5f727694f2827b61960f929fc846d1e8
Nottrex <Lau.Nottrex@gmail.com>
2018-03-02 08:23:45 +0100

    Update Drawable.java

--- src/game/window/Drawable.java
+++ src/game/window/Drawable.java
@@ -1,11 +1,31 @@
 package game.window;
 
 public interface Drawable {
+	/**
+	 * the drawing priority determines how early the drawable is drawn
+	 * therefore this is equal to the depth -> higher priority means
+	 * this is drawn behind the other drawables
+	 * @return the drawing priority
+	 */
 	float getDrawingPriority();
 
+	/**
+	 * executed, when this drawable is added to a window
+	 * @param window the window this has been added to
+	 */
 	void setup(Window window);
 
+	/**
+	 * executed every time the window repaints
+	 * therefore this object has to draw itself
+	 * @param window the window this has been added to
+	 * @param time the time (in ms) of the repaint
+	 */
 	void draw(Window window, long time);
 
+	/**
+	 * executed, when this drawable is removed from a window
+	 * @param window the window this is removed from
+	 */
 	void cleanUp(Window window);
 }
